46ce7057eeb374b44d2648441c761c2ccef75792
Anvesh <atvanvesh@gmail.com>
2024-07-31 14:40:17 -0500

    env files as well as fixes

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDSEFcHJ2U6pENJRmcWDQOsbOkk0WXm6QM",
-  authDomain: "inventory-app-238d5.firebaseapp.com",
-  projectId: "inventory-app-238d5",
-  storageBucket: "inventory-app-238d5.appspot.com",
-  messagingSenderId: "422836340520",
-  appId: "1:422836340520:web:0e3ebc963f30aeae72b21d",
-  measurementId: "G-7WVBHHJPLH",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
