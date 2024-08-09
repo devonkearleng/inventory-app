dc53b8b0a27ea4841b8a5353d56e4f76f3615984
Devon Kear-Leng <devonkear11@gmail.com>
2024-08-09 15:15:17 -0500

    refac: deleted out comments

--- src/app/LandingPage.js
+++ src/app/LandingPage.js
@@ -1,61 +1,3 @@
-// "use client";
-
-// import { useRouter } from "next/navigation";
-// import { auth, provider, signInWithPopup } from "@/firebase";
-// import { Box, Button, Typography } from "@mui/material";
-// import { useEffect, useState } from "react";
-
-// const LandingPage = () => {
-//   const router = useRouter();
-//   const [error, setError] = useState(null);
-
-//   const handleGoogleSignIn = async () => {
-//     try {
-//       const result = await signInWithPopup(auth, provider);
-//       const user = result.user;
-//       sessionStorage.setItem(
-//         "user",
-//         JSON.stringify({ name: user.displayName, uid: user.uid })
-//       );
-//       router.push("/dashboard");
-//     } catch (error) {
-//       setError(error.message);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const user = JSON.parse(sessionStorage.getItem("user"));
-//     if (user) {
-//       router.push("/dashboard");
-//     }
-//   }, [router]);
-
-//   return (
-//     <Box
-//       display="flex"
-//       flexDirection="column"
-//       alignItems="center"
-//       justifyContent="center"
-//       height="100vh"
-//       bgcolor="#f5f5f5"
-//     >
-//       <Typography variant="h4" mb={2}>
-//         Sign In with Google
-//       </Typography>
-//       <Button variant="contained" onClick={handleGoogleSignIn}>
-//         Sign In with Google
-//       </Button>
-//       {error && (
-//         <Typography color="error" mt={2}>
-//           {error}
-//         </Typography>
-//       )}
-//     </Box>
-//   );
-// };
-
-// export default LandingPage;
-
 "use client";
 
 import { useRouter } from "next/navigation";
@@ -63,6 +5,9 @@ import { auth, provider, signInWithPopup } from "@/firebase";
 import { Box, Button, Typography, Paper } from "@mui/material";
 import { useEffect, useState } from "react";
 
+/**
+ * This is the landing page, where we handle signing in to a user's Google account.
+ */
 const LandingPage = () => {
   const router = useRouter();
   const [error, setError] = useState(null);
